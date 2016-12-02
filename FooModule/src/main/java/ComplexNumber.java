6fbb601f5388572655ceb8ddb8ca246105a8b556
Your Name <briksmith@earthlink.net>
2016-12-02 12:19:33 -0600

    New return sum method
    I want a method the returns the sum of 2 complex numbers.  Made the obvious test for it.
    
    I made it a static method, as it does not depend on its own state.

--- FooModule/src/main/java/ComplexNumber.java
+++ FooModule/src/main/java/ComplexNumber.java
@@ -18,6 +18,21 @@ public class ComplexNumber {
         this.imaginary += inComplexNumber.imaginary;
     }
 
+    public static ComplexNumber returnSumOfComplexNumbers(ComplexNumber num1, ComplexNumber num2){
+
+        double newRealComponent = getNewRealComponent(num1, num2);
+        double newImaginaryComponent = getNewImaginaryComponent(num1, num2);
+        return new ComplexNumber(newRealComponent, newImaginaryComponent);
+    }
+
+    private static double getNewRealComponent(ComplexNumber num1, ComplexNumber num2) {
+        return num1.getReal() + num2.getReal();
+    }
+
+    private static double getNewImaginaryComponent(ComplexNumber num1, ComplexNumber num2) {
+        return num1.getImaginary() + num2.getImaginary();
+    }
+
     public double getReal() {
         return real;
     }
--- FooModule/src/test/groovy/TestComplexNumbers.groovy
+++ FooModule/src/test/groovy/TestComplexNumbers.groovy
@@ -8,7 +8,7 @@ class TestConstructorOfComplexNumbers extends Specification {
 
     def i = new ComplexNumber(1,3)
 
-    def "constructorCheck"() {
+    def "checkConstructor"() {
         expect: assert i.getReal() == 1 :  "The constructor should have set the real component of the number to 1 and it set it to " + i.getReal()
             assert i.getImaginary() == 3 : "The constructor should have set the imaginary component of the number to 3 and it set it to " + i.getImaginary()
     }
@@ -16,14 +16,25 @@ class TestConstructorOfComplexNumbers extends Specification {
 
 class TestAddToComplexNumber extends Specification {
 
-    ComplexNumber i = new ComplexNumber(2,4)
-    ComplexNumber j = new ComplexNumber(4,9)
+    ComplexNumber i = new ComplexNumber(2, 4)
+    ComplexNumber j = new ComplexNumber(4, 9)
 
-    def "addToComplexNumberCheck" () {
+    def "checkAddToComplexNumber"() {
         i.addToComplex(j)
         expect:
-            assert i.getReal() == 6 : "The add to complex number routine should have set the real component to 6 and set it to " + i.getReal();
-            assert i.getImaginary() == 13 : "The add to complex number routine should have set the imaginary component to 13 and set it to " + i.getImaginary();
+        assert i.getReal() == 6: "The add to complex number routine should have set the real component to 6 and set it to " + i.getReal();
+        assert i.getImaginary() == 13: "The add to complex number routine should have set the imaginary component to 13 and set it to " + i.getImaginary();
     }
+}
+class TestReturnSumOfComplexNumber extends Specification {
 
+    ComplexNumber i = new ComplexNumber(3,6)
+    ComplexNumber j = new ComplexNumber(4, 12)
+
+    def "checkReturnSumOfComplexNumber" () {
+        ComplexNumber checkSum = ComplexNumber.returnSumOfComplexNumbers(i,j)
+        expect:
+            assert checkSum.getReal() == 7 : "The return sumOfComplexNumbers method should have returned a complex number with 7 as its real component.  Was: " + checkSum.getReal()
+            assert checkSum.getImaginary() == 18 : "The return sumOfComplexNumbers method should have returned a complex number with 18 as its imaginary component.  Was: " + checkSum.getImaginary()
+    }
 }
